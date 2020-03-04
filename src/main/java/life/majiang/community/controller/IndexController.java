b50a20da8f5772ec42fcc885dd2d4f93f7a34d41
yushuang <1239993948@qq.com>
2020-03-04 23:25:26 +0800

    2020 3/4 页面初始搭建完成

--- src/main/java/life/majiang/community/controller/IndexController.java
+++ src/main/java/life/majiang/community/controller/IndexController.java
@@ -6,12 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class HelloController {
+public class IndexController {
 
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name = "name") String name , Model model){
-        model.addAttribute("name",name);
-        return "hello";
+    @GetMapping("/")//表示根目录 只要输入localhost:8887/就可以进入
+    public String index(){
+
+        return "index";
     }
 
 }
